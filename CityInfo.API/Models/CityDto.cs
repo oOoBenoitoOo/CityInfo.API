508cad14166dd96aabc331a7a3cd411c05eabf61
Benoit <nau.benoit@gmail.com>
2023-03-10 18:55:54 -0400

    Controllers + Models + Files

--- CityInfo.API/Models/CityDto.cs
+++ CityInfo.API/Models/CityDto.cs
@@ -6,6 +6,16 @@
         public string Name { get; set; } = String.Empty;
         public string? Description { get; set; }
 
+        public int NumberOfPointsOfInterest
+        {
+            get
+            {
+                return PointsOfInterest.Count;
+            }
+        }
+
+        public ICollection<PointOfInterestDto> PointsOfInterest { get; set; }
+            = new List<PointOfInterestDto>();
 
     }
 }
